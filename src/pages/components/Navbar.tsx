fcacfe48f6d9814982ac612f2e614d88708ec307
Ing-China <peschina6677@gmail.com>
2025-03-12 23:23:27 +0700

    added stack data

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -5,7 +5,6 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 const navLinks = [
   { title: "About", path: "#about" },
   { title: "Stack", path: "#stack" },
-  { title: "Experience", path: "#experience" },
   { title: "Project", path: "#project" },
   { title: "Contact", path: "#contact" },
 ];
@@ -19,7 +18,7 @@ const Navbar = () => {
   };
   return (
     <div className="z-50 fixed flex justify-center w-full text-white font-bold">
-      <div className="border border-white/20 mt-8 backdrop-blur-3xl rounded-lg hidden md:flex items-center justify-center p-2 max-w-[500px] mx-auto">
+      <div className="border border-white/20 mt-8 backdrop-blur-3xl rounded-lg hidden md:flex items-center justify-center p-2 max-w-[400px] mx-auto">
         <ul className="flex flex-row p-2 space-x-8">
           {navLinks.map((link, index) => (
             <li key={index}>
